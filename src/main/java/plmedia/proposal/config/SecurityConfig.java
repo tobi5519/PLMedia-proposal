b8e093ada8b9abc134df8a6da0247154d5bf5ed3
Unknown <theisko@gmail.com>
2018-05-08 11:08:35 +0200

    ..og igen

--- src/main/java/plmedia/proposal/config/SecurityConfig.java
+++ src/main/java/plmedia/proposal/config/SecurityConfig.java
@@ -35,7 +35,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers(  "/css/**", "/img/**", "login").permitAll() // Permission: Anyone
+                .antMatchers(   "/images/**", "login").permitAll() // Permission: Anyone
                 .anyRequest().authenticated().and().formLogin().loginPage("/login").permitAll().loginProcessingUrl("/login").defaultSuccessUrl("/index", true).and().logout()
                 .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
                 .logoutSuccessUrl("/login").permitAll()
