70169f1559e3df7ee2431ca2888b11cca0c74907
Unknown <theisko@gmail.com>
2018-05-14 16:27:34 +0200

    One can now compare statistics of two periods

--- src/main/java/plmedia/proposal/controller/MainController.java
+++ src/main/java/plmedia/proposal/controller/MainController.java
@@ -9,6 +9,9 @@ import plmedia.proposal.model.entities.*;
 import plmedia.proposal.model.repositories.*;
 import plmedia.proposal.model.services.*;
 
+import java.sql.Date;
+import java.util.List;
+
 @Controller
 public class MainController {
 
@@ -68,6 +71,22 @@ public class MainController {
         model.addAttribute("totalProposals", Double.valueOf(proposalRepo.findAll().size()));
         model.addAttribute("totalAcceptedProposals", Double.valueOf(proposalRepo.findAllByAcceptDateIsNotNull().size()));
         model.addAttribute("totalNotAcceptedProposals", Double.valueOf(proposalRepo.findAllByAcceptDateIsNull().size()));
+
+        return "statistics";
+    }
+
+    @RequestMapping(value = {"statistics"}, method = RequestMethod.POST)
+    public String statistics(@RequestParam("date") List<Date> dates, Model model) {
+
+        // Period 1
+        model.addAttribute("totalProposals", Double.valueOf(proposalRepo.findAllBySendDateBetween(dates.get(0), dates.get(1)).size()));
+        model.addAttribute("totalAcceptedProposals", Double.valueOf(proposalRepo.findAllByAcceptDateIsNotNullAndSendDateIsBetween(dates.get(0), dates.get(1)).size()));
+        model.addAttribute("totalNotAcceptedProposals", Double.valueOf(proposalRepo.findAllByAcceptDateIsNullAndSendDateIsBetween(dates.get(0), dates.get(1)).size()));
+
+        // Period 2
+        model.addAttribute("totalProposals1", Double.valueOf(proposalRepo.findAllBySendDateBetween(dates.get(2), dates.get(3)).size()));
+        model.addAttribute("totalAcceptedProposals1", Double.valueOf(proposalRepo.findAllByAcceptDateIsNotNullAndSendDateIsBetween(dates.get(2), dates.get(3)).size()));
+        model.addAttribute("totalNotAcceptedProposals1", Double.valueOf(proposalRepo.findAllByAcceptDateIsNullAndSendDateIsBetween(dates.get(2), dates.get(3)).size()));
 
         return "statistics";
     }
--- src/main/java/plmedia/proposal/model/repositories/ProposalRepo.java
+++ src/main/java/plmedia/proposal/model/repositories/ProposalRepo.java
@@ -3,10 +3,15 @@ package plmedia.proposal.model.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import plmedia.proposal.model.entities.Proposal;
 
+import java.sql.Date;
 import java.util.List;
 
 public interface ProposalRepo extends JpaRepository<Proposal, Integer> {
     List<Proposal> findAll();
     List<Proposal> findAllByAcceptDateIsNotNull();
     List<Proposal> findAllByAcceptDateIsNull();
+    List<Proposal> findAllBySendDateBetween(Date fromDate, Date toDate);
+    List<Proposal> findAllByAcceptDateIsNotNullAndSendDateIsBetween(Date fromDate, Date toDate);
+    List<Proposal> findAllByAcceptDateIsNullAndSendDateIsBetween(Date fromDate, Date toDate);
+
 }
